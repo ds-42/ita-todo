139300a69d5a63de5956aeba6e5ad4c6bf74efb5
ASUS\Aleks <lai@tut.by>
2024-02-24 23:03:58 +0300

    todo list

---
+++ ToDo/Models/Todo.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Label { get; set; } = string.Empty;
+        public bool IsDone { get; set; } = false;
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+        public static Todo Create(int id, string label) 
+        {
+            return new Todo 
+            { 
+                Id = id, 
+                Label = label,
+                IsDone = false,
+                CreateDate = DateTime.UtcNow,
+                UpdateDate = DateTime.UtcNow,
+            };
+        }
+    }
+}
